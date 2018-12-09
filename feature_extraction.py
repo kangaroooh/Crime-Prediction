f89bf102b8469e7ed950bc9af43b28f2da09f6b6
robroooh <robroo.pc@gmail.com>
2018-12-09 16:46:35 +0200

    finish negative reviews

--- feature_extraction.py
+++ feature_extraction.py
@@ -15,6 +15,8 @@ from tqdm import tqdm
 #%%
 hotel_reviews = pd.read_excel("combine_123_allcols.xlsx")
 title_sentiment = pd.read_excel("title_sentiment.xlsx")
+hotel_reviews["Sentiment"] = title_sentiment
+hotel_reviews["Hotel Name"] = hotel_reviews["Hotel Name"].apply(lambda x: x.strip())
 #%%
 """
 Compute Class for classification based on Sentiment Analysis and Review Stars
@@ -22,8 +24,6 @@ We believe that if those two don't go together, they might mess up our
 analysis later, to we exclude them (making them -1)
 """
 review_stars = hotel_reviews["Review Stars"]
-#%%
-hotel_reviews["Sentiment"] = title_sentiment
 #%%
 def compute_class(x):
     star = x["Review Stars"]
@@ -39,7 +39,6 @@ def compute_class(x):
         ret = -1
     return ret
 hotel_reviews_class = hotel_reviews.apply(compute_class, axis=1)
-#%%
 hotel_reviews["Class"] = hotel_reviews_class
 #%%
 """
@@ -67,12 +66,10 @@ maxdate_reviewer.columns = ["maxdate"]
 mindate_reviewer = hotel_reviews[["Reviewer Name", "Review Date"]].groupby("Reviewer Name").min()
 mindate_reviewer.columns = ["mindate"]
 user_maxmindate = pd.concat([maxdate_reviewer, mindate_reviewer], axis=1, sort=True)
-#%%
 user_maxmindate["datediff"] = user_maxmindate["maxdate"] - user_maxmindate["mindate"]
 #%%
-user_maxmindate["datediff"].describe()
 """
-ComputeEffective Star Rating, to enhance the current Star Ratings with score
+Feature Extraction: ComputeEffective Star Rating, to enhance the current Star Ratings with score
 from Sentiment Analysis. Due to the fact that people have their own weighting
 justification. The 3 of some people might mean Neutral, while some mean bad.
 """
@@ -90,15 +87,53 @@ def compute_effective_star_rating(x):
         ret = None
     return ret
 hotel_effective_star_rating = hotel_reviews.apply(compute_effective_star_rating, axis=1)
-#%%
 hotel_reviews["Effective Star Rating"] = hotel_effective_star_rating
+#%%
 """
 Using Effective Star Rating to validate if our Effective Star rating
 is actually good by comparing it with the raw star rating
 """
-#%%
 hotel_rates = hotel_reviews[["Hotel Name", "Hotel Review Stars", "Effective Star Rating"]].groupby("Hotel Name").mean()
-#%%
 eq = hotel_rates[hotel_rates["Hotel Review Stars"] == hotel_rates["Effective Star Rating"]]
 mt = hotel_rates[hotel_rates["Hotel Review Stars"] > hotel_rates["Effective Star Rating"]]
 lt = hotel_rates[hotel_rates["Hotel Review Stars"] < hotel_rates["Effective Star Rating"]]
+hotel_reviews["Hotel Name"].nunique()
+
+#%%
+"""
+Finding correlation between hotel & Crime
+"""
+#%%
+unique_hotel = hotel_reviews[["Hotel Name","Latitude","Longitude"]].groupby("Hotel Name").first()
+#%%
+crime_data = pd.read_csv("crimedata/2018-08-metropolitan-street.csv")
+#%%
+unique_hotel["Latitude"] = pd.to_numeric(unique_hotel["Latitude"], errors='coerce')
+unique_hotel["Longitude"] = pd.to_numeric(unique_hotel["Longitude"], errors='coerce')
+result = pd.merge(unique_hotel, crime_data, how='left', on=["Latitude","Longitude"])
+
+#%%
+"""
+Feature Extraction: Finding % of negative reviews.
+"""
+hotel_reviews["Hotel Name"] = hotel_reviews["Hotel Name"].apply(lambda x: x.strip())
+#%%
+grouped_hotelname = hotel_reviews[["Hotel Name", "Sentiment"]].groupby(
+        "Hotel Name")
+#%%
+def smi(x):
+    vcount = x["Sentiment"].value_counts()
+    summi = 0
+    if 'Negative' in vcount:
+        summi += vcount['Negative']
+    if 'Verynegative' in vcount:
+        summi += vcount['Verynegative']
+    return summi
+negrev = grouped_hotelname.agg(smi)
+allrev = grouped_hotelname.agg('count')
+ab = pd.merge(negrev, allrev, on="Hotel Name")
+ab.columns = ["nb_negreview", "nb_allreview"]
+#%%
+hotel_reviews = pd.merge(hotel_reviews, ab, left_on="Hotel Name", right_on="Hotel Name")
+#%%
+hotel_reviews.to_excel("negreviews.xlsx")
